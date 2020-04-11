2fa9953a90a7ec6e5bd3256604620788b9a3441a
neelkirit <neelkirit2@gmail.com>
2020-04-11 18:58:43 -0400

    Fixed db
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,7 +5,7 @@ from flask import g
 
 def get_db():
     if 'db' not in g:
-        g.db = pymysql.connect(db='hangouts', user='root', passwd='m@d@rc#0d', host='35.229.20.55', port=3306)
+        g.db = pymysql.connect(db='hangout', user='root', passwd='m@d@rc#0d', host='35.229.20.55', port=3306)
     return g.db
 
 
